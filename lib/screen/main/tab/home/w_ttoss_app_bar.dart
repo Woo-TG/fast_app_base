8782ea53bde4bdecded104cf0f08dc10af260a83
wtg <yawhy1234@naver.com>
2024-01-23 06:33:55 +0900

    01-23 home

--- lib/screen/main/tab/home/w_ttoss_app_bar.dart
+++ lib/screen/main/tab/home/w_ttoss_app_bar.dart
@@ -13,6 +13,7 @@ class TtossAppBar extends StatefulWidget {
 
 class _TtossAppBarState extends State<TtossAppBar> {
   bool _showRedDot = false;
+  int _tappingCount = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +23,34 @@ class _TtossAppBarState extends State<TtossAppBar> {
       child: Row(
         children: [
           width10,
-          Image.asset('$basePath/icon/toss.png', height: 30),
+          AnimatedContainer(
+              duration: 1000.ms,
+              // curve: Curves.easeIn,
+              color: _tappingCount > 2 ? Colors.red : Colors.blue,
+              // width: 70,
+              height: _tappingCount > 2 ? 60 : 30,
+              child: Image.asset('$basePath/icon/toss.png')),
+          AnimatedCrossFade(
+              firstChild: Image.asset(
+                '$basePath/icon/toss.png',
+                height: 30,
+              ),
+              secondChild: Image.asset(
+                '$basePath/icon/map_point.png',
+                height: 30,
+              ),
+              crossFadeState: _tappingCount < 2
+                  ? CrossFadeState.showSecond
+                  : CrossFadeState.showSecond,
+              duration: 1500.ms),
           emptyExpanded,
-          Image.asset('$basePath/icon/map_point.png', height: 30),
+          Tap(
+              onTap: () {
+                setState(() {
+                  _tappingCount++;
+                });
+              },
+              child: Image.asset('$basePath/icon/map_point.png', height: 30)),
           width10,
           Tap(
             onTap: () {
@@ -48,7 +74,7 @@ class _TtossAppBarState extends State<TtossAppBar> {
                   ),
               ],
             )
-                .animate()   // onPlay: (controller) => controller.repeat()
+                .animate() // onPlay: (controller) => controller.repeat()
                 .shake(duration: 2000.ms, hz: 3)
                 .then()
                 .fadeOut(duration: 1000.ms),
