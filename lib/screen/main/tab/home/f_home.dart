c207b06574de7d269f78700f9247a4fdaad9c090
userpc777 <yawhy1234@naver.com>
2024-01-24 18:18:11 +0900

    01-24 office

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -3,6 +3,7 @@ import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
 import 'package:fast_app_base/screen/main/tab/home/vo/bank_accounts_dummy.dart';
 import 'package:fast_app_base/screen/main/tab/home/vo/w_bank_account.dart';
+import 'package:fast_app_base/screen/main/tab/home/vo/w_rive_like_button.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_ttoss_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:live_background/live_background.dart';
@@ -13,11 +14,17 @@ import '../../../dialog/d_color_bottom.dart';
 import '../../../dialog/d_confirm.dart';
 import '../../s_main.dart';
 
-
-class HomeFragment extends StatelessWidget {
+class HomeFragment extends StatefulWidget {
   const HomeFragment({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<HomeFragment> createState() => _HomeFragmentState();
+}
+
+class _HomeFragmentState extends State<HomeFragment> {
+  bool isLike = false;
 
   @override
   Widget build(BuildContext context) {
@@ -32,31 +39,41 @@ class HomeFragment extends StatelessWidget {
             ),
             RefreshIndicator(
               edgeOffset: TtossAppBar.appBarHeight,
-              onRefresh: () async{
+              onRefresh: () async {
                 await sleepAsync(500.ms);
               },
               child: SingleChildScrollView(
                 padding: const EdgeInsets.only(
-                    top: TtossAppBar.appBarHeight, bottom: MainScreenState.bottomNavigatorHeight),
+                    top: TtossAppBar.appBarHeight,
+                    bottom: MainScreenState.bottomNavigatorHeight),
                 child: Column(
                   children: [
-                    BigButton('토스뱅크',
+                    SizedBox(height: 250, width: 250, child: RiveLikeButton(isLike, onTapLike: (isLike){
+                      setState(() {
+                        this.isLike = isLike;
+                      });
+                    },)),
+                    BigButton(
+                      '토스뱅크',
                       onTap: () {
                         context.showSnackbar('토스뱅크를 눌렀어요');
-                      },),
+                      },
+                    ),
                     height10,
                     RoundedContainer(
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            '자산'.text.bold.white.make(),
-                            height5,
-                            ...bankAccounts.map((e) => BankAccountWidget(e)).toList()
-                          ],
-                        )
-                    )
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        '자산'.text.bold.white.make(),
+                        height5,
+                        ...bankAccounts
+                            .map((e) => BankAccountWidget(e))
+                            .toList()
+                      ],
+                    ))
                   ],
-                ).pSymmetric(h: 20).animate().slideY(duration: 3000.ms).fadeIn(),
+                ).pSymmetric(
+                    h: 20) /*.animate().slideY(duration: 3000.ms).fadeIn()*/,
               ),
             ),
             const TtossAppBar()
